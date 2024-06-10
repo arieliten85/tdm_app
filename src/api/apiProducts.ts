6dbc3c1e7e0f34b7b88ee3b107ed3738b0c5e42e
arielferen <arieliten85@gmail.com>
2024-06-10 18:29:54 -0300

    feat: update api

--- src/api/apiProducts.ts
+++ src/api/apiProducts.ts
@@ -1,85 +1,7 @@
-// export const apiProducts = [
-//   {
-//     id: "01",
-//     img: "https://cuk-it.com/wp-content/uploads/2020/06/alfajores-maicena.webp",
-//     title: "Alfajores de maicena",
-//     description:
-//       "Nuestros alfajores de maicena combinan la suavidad de la masa con un exquisito relleno de dulce de leche, todo cubierto por una capa de coco rallado. ¡Ordénalos ahora y disfruta de una experiencia de sabor única!",
-//     price: "$2.50",
-//     tags: ["maicena", "coco", "dulce", "seco", "masas", "dulce de leche"],
-//   },
-//   {
-//     id: "02",
-//     img: "https://images.aws.nestle.recipes/resized/2020_06_03T13_21_43_mrs_ImageRecipes_147427lrg_1080_850.jpg",
-//     title: "Brownie Tentación",
-//     description:
-//       "Nuestro Brownie Tentación es una delicia irresistible con una textura densa y rica en chocolate, complementada con chips de chocolate. Perfecto para disfrutar en cualquier momento del día.",
-//     price: "$3.00",
-//     tags: ["brownie", "chocolate", "nuez"],
-//   },
-//   {
-//     id: "03",
-//     img: "https://www.locosxelmate.com/wp-content/uploads/2015/11/pasta_frola.jpg",
-//     title: "Pastafrola Artesanal",
-//     description:
-//       "Disfruta de nuestra Pastafrola Artesanal, hecha con una crujiente base y un delicioso relleno de dulce de membrillo casero. ¡Cada bocado te transportará a los sabores tradicionales!",
-//     price: "$4.50",
-//     tags: ["maza", "membrillo", "vainilla"],
-//   },
-//   {
-//     id: "04",
-//     img: "https://www.johaprato.com/files/styles/flexslider_full/public/budin_vainilla.jpg?itok=BjnPfhTA",
-//     title: "Budín de Vainilla",
-//     description:
-//       "Nuestro Budín de Vainilla es una delicia clásica, con una textura suave y un sabor a vainilla que te encantará. Perfecto para acompañar con una taza de té o café.",
-//     price: "$3.50",
-//     tags: ["Budín", "vainilla"],
-//   },
-//   {
-//     id: "05",
-//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_V38fYdTpZZZWBKm2zsmdl2TLhkSowMgietl9YwQg65Bz0p2ltsrousuxyvW6aGwTG8c&usqp=CAU",
-//     title: "Galletitas",
-//     description:
-//       "Revive tus mejores recuerdos con nuestras Galletitas caseras, llenas de chispas de chocolate y horneadas con amor. Perfectas para acompañar tu café o té. ¡Ordénalas y disfruta del sabor nostálgico!",
-//     price: "$2.50",
-//     tags: ["dulce", "galletitas", "vainilla"],
-//   },
-//   {
-//     id: "06",
-//     img: "https://resizer.glanacion.com/resizer/v2/cuadrados-de-coco-y-dulce-de-leche-por-antonio-MZI5W3E4RNCY5MPRVD3SU45ISA.jpg?auth=8ba6531479a294314554a75b3b32299267b2ff23121947c09968c6fca0c7bb82&width=768&height=512&quality=70&smart=true",
-//     title: "Cuadrados de coco",
-//     description:
-//       "Nuestros Cuadrados de Coco combinan una base dulce con un generoso relleno de dulce de leche y una capa superior de coco rallado. ¡Una delicia que no puedes dejar de probar!",
-//     price: "$3.00",
-//     tags: ["dulce", "coco", "dulce de leche"],
-//   },
-//   {
-//     id: "07",
-//     img: "https://cuk-it.com/wp-content/uploads/2021/09/budin-chocolate.webp",
-//     title: "Budín de chocolate",
-//     description:
-//       "Nuestro Budín de Chocolate es una explosión de sabor con cada mordisco. Hecho con chips de chocolate, este budín te transportará a los sabores de tu infancia. ¡Ordénalo y disfruta!",
-//     price: "$4.50",
-//     tags: ["dulce", "budin", "chocolate"],
-//   },
-//   {
-//     id: "08",
-//     img: "https://www.clarin.com/2021/05/14/KmxHFgxit_1200x0__1.jpg",
-//     title: "Pastafrola de batata Tradicional",
-//     description:
-//       "Nuestra Pastafrola de Batata Tradicional, con su base crocante y dulce de batata artesanal, te ofrece una experiencia de sabor auténtica. ¡Ordénala y disfruta cada mordisco!",
-//     price: "$6.50",
-//     tags: ["dulce", "batata"],
-//   },
-// ];
-
-import image01 from "../assets/productos/imagen01.jpg";
-import image02 from "../assets/productos/imagen02.jpg";
-import image03 from "../assets/productos/imagen03.jpg";
 export const apiProducts = [
   {
     id: "01",
-    img: image01,
+    img: "https://cuk-it.com/wp-content/uploads/2020/06/alfajores-maicena.webp",
     title: "Alfajores de maicena",
     description:
       "Nuestros alfajores de maicena combinan la suavidad de la masa con un exquisito relleno de dulce de leche, todo cubierto por una capa de coco rallado. ¡Ordénalos ahora y disfruta de una experiencia de sabor única!",
@@ -88,7 +10,7 @@ export const apiProducts = [
   },
   {
     id: "02",
-    img: image02,
+    img: "https://images.aws.nestle.recipes/resized/2020_06_03T13_21_43_mrs_ImageRecipes_147427lrg_1080_850.jpg",
     title: "Brownie Tentación",
     description:
       "Nuestro Brownie Tentación es una delicia irresistible con una textura densa y rica en chocolate, complementada con chips de chocolate. Perfecto para disfrutar en cualquier momento del día.",
@@ -97,7 +19,7 @@ export const apiProducts = [
   },
   {
     id: "03",
-    img: image03,
+    img: "https://www.locosxelmate.com/wp-content/uploads/2015/11/pasta_frola.jpg",
     title: "Pastafrola Artesanal",
     description:
       "Disfruta de nuestra Pastafrola Artesanal, hecha con una crujiente base y un delicioso relleno de dulce de membrillo casero. ¡Cada bocado te transportará a los sabores tradicionales!",
@@ -150,3 +72,81 @@ export const apiProducts = [
     tags: ["dulce", "batata"],
   },
 ];
+
+// import image01 from "../assets/productos/imagen01.jpg";
+// import image02 from "../assets/productos/imagen02.jpg";
+// import image03 from "../assets/productos/imagen03.jpg";
+// export const apiProducts = [
+//   {
+//     id: "01",
+//     img: image01,
+//     title: "Alfajores de maicena",
+//     description:
+//       "Nuestros alfajores de maicena combinan la suavidad de la masa con un exquisito relleno de dulce de leche, todo cubierto por una capa de coco rallado. ¡Ordénalos ahora y disfruta de una experiencia de sabor única!",
+//     price: "$2.50",
+//     tags: ["maicena", "coco", "dulce", "seco", "masas", "dulce de leche"],
+//   },
+//   {
+//     id: "02",
+//     img: image02,
+//     title: "Brownie Tentación",
+//     description:
+//       "Nuestro Brownie Tentación es una delicia irresistible con una textura densa y rica en chocolate, complementada con chips de chocolate. Perfecto para disfrutar en cualquier momento del día.",
+//     price: "$3.00",
+//     tags: ["brownie", "chocolate", "nuez"],
+//   },
+//   {
+//     id: "03",
+//     img: image03,
+//     title: "Pastafrola Artesanal",
+//     description:
+//       "Disfruta de nuestra Pastafrola Artesanal, hecha con una crujiente base y un delicioso relleno de dulce de membrillo casero. ¡Cada bocado te transportará a los sabores tradicionales!",
+//     price: "$4.50",
+//     tags: ["maza", "membrillo", "vainilla"],
+//   },
+//   {
+//     id: "04",
+//     img: "https://www.johaprato.com/files/styles/flexslider_full/public/budin_vainilla.jpg?itok=BjnPfhTA",
+//     title: "Budín de Vainilla",
+//     description:
+//       "Nuestro Budín de Vainilla es una delicia clásica, con una textura suave y un sabor a vainilla que te encantará. Perfecto para acompañar con una taza de té o café.",
+//     price: "$3.50",
+//     tags: ["Budín", "vainilla"],
+//   },
+//   {
+//     id: "05",
+//     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_V38fYdTpZZZWBKm2zsmdl2TLhkSowMgietl9YwQg65Bz0p2ltsrousuxyvW6aGwTG8c&usqp=CAU",
+//     title: "Galletitas",
+//     description:
+//       "Revive tus mejores recuerdos con nuestras Galletitas caseras, llenas de chispas de chocolate y horneadas con amor. Perfectas para acompañar tu café o té. ¡Ordénalas y disfruta del sabor nostálgico!",
+//     price: "$2.50",
+//     tags: ["dulce", "galletitas", "vainilla"],
+//   },
+//   {
+//     id: "06",
+//     img: "https://resizer.glanacion.com/resizer/v2/cuadrados-de-coco-y-dulce-de-leche-por-antonio-MZI5W3E4RNCY5MPRVD3SU45ISA.jpg?auth=8ba6531479a294314554a75b3b32299267b2ff23121947c09968c6fca0c7bb82&width=768&height=512&quality=70&smart=true",
+//     title: "Cuadrados de coco",
+//     description:
+//       "Nuestros Cuadrados de Coco combinan una base dulce con un generoso relleno de dulce de leche y una capa superior de coco rallado. ¡Una delicia que no puedes dejar de probar!",
+//     price: "$3.00",
+//     tags: ["dulce", "coco", "dulce de leche"],
+//   },
+//   {
+//     id: "07",
+//     img: "https://cuk-it.com/wp-content/uploads/2021/09/budin-chocolate.webp",
+//     title: "Budín de chocolate",
+//     description:
+//       "Nuestro Budín de Chocolate es una explosión de sabor con cada mordisco. Hecho con chips de chocolate, este budín te transportará a los sabores de tu infancia. ¡Ordénalo y disfruta!",
+//     price: "$4.50",
+//     tags: ["dulce", "budin", "chocolate"],
+//   },
+//   {
+//     id: "08",
+//     img: "https://www.clarin.com/2021/05/14/KmxHFgxit_1200x0__1.jpg",
+//     title: "Pastafrola de batata Tradicional",
+//     description:
+//       "Nuestra Pastafrola de Batata Tradicional, con su base crocante y dulce de batata artesanal, te ofrece una experiencia de sabor auténtica. ¡Ordénala y disfruta cada mordisco!",
+//     price: "$6.50",
+//     tags: ["dulce", "batata"],
+//   },
+// ];
